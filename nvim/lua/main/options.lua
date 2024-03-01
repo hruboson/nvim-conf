8dbd8e142b7e8cb4eca539d0680ae028653835d2
Ondřej Hruboš <hruboson@gmail.com>
2024-03-01 13:36:26 +0100

    Added first bit of settings and used fonts

---
+++ nvim/lua/main/options.lua
@@ -0,0 +1,5 @@
+vim.opt.clipboard = "unnamedplus" 	-- set windows clipboard
+vim.opt.ignorecase = true
+vim.opt.number = true			-- show line numbers
+vim.opt.mouse = "a"			-- set default mouse action
+vim.opt.wrap = false			-- do not wrap long lines
