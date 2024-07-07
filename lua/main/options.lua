7dd813afffef273f1072bd64a74c371be9089c42
Ondřej Hruboš <hruboson@gmail.com>
2024-07-07 13:52:47 +0200

    Useful options

--- lua/main/options.lua
+++ lua/main/options.lua
@@ -1,7 +1,14 @@
 vim.opt.clipboard = "unnamedplus" 		-- set os clipboard
 vim.opt.ignorecase = true
 vim.opt.number = true					-- show line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%s %l %r "
 vim.opt.mouse = "a"						-- set default mouse action
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
 vim.opt.wrap = false					-- do not wrap long lines
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
