3cf4726f6b18743f9a07fc294dc76a6610758bc0
Ondřej Hruboš <hruboson@gmail.com>
2024-07-14 15:58:50 +0200

    Revert "Which-key does weird stuff"
    This reverts commit 1f42cc50a81f2b739d0574f0005cbdce8a50f199.

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -107,22 +107,4 @@ require("lazy").setup({ -- Enable Lazy
     		{"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
   		},
 	},
-	{
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
-	-- refer to the configuration section below
-	},
-	keys = {
-		{
-		"<C-'>",
-		function()
-			require("which-key").show({ global = false })
-		end,
-		desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
-}
 })
