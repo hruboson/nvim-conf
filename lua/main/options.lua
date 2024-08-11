5d1292dd209f96f3cc419d1e1f156fc2e74c730f
Ondřej Hruboš <hruboson@gmail.com>
2024-08-11 13:18:26 +0200

    Tweaks in options

--- lua/main/options.lua
+++ lua/main/options.lua
@@ -4,7 +4,7 @@ vim.opt.ignorecase = true
 vim.opt.number = true					-- show line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.o.statuscolumn = "%s %l %r "
+-- vim.o.statuscolumn = "%s %l %r "
 vim.opt.mouse = "a"						-- set default mouse action
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
@@ -13,20 +13,7 @@ vim.opt.hlsearch = true
 vim.opt.wrap = false					-- do not wrap long lines
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.fillchars = {eob = " "}			-- remove tildes on blanklines
 vim.opt.mousemoveevent = true 			-- should allow tabs to hover but doesnt work for some reason ://
-vim.cmd[[autocmd FileType * set formatoptions-=cro]] -- disable continuous comments on new line 
+--vim.cmd[[autocmd FileType * set formatoptions-=cro]] -- disable continuous comments on new line 
 
--- COQ settings
-vim.g.coq_settings = {
-  keymap = {
-    recommended = false,
-    jump_to_mark = '',
-    pre_select = true,
-  },
-  auto_start = true,
-  display = {
-    pum = {
-      fast_close = false
-    }
-  }
-}
