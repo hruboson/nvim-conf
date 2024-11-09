640d88291c71daaa89c0d1e6c8dbd60ce9f51e11
Ondřej Hruboš <hruboson@gmail.com>
2024-11-09 12:16:31 +0100

    change markdown preview plugin

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -94,10 +94,14 @@ require("lazy").setup({ -- Enable Lazy
 	{'b0o/mapx.nvim'}, -- easy keybinds
 	{'dstein64/nvim-scrollview'}, -- scrollbar
 	{ -- Markdown preview
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function() vim.fn["mkdp#util#install"]() end,
+		"OXY2DEV/markview.nvim",
+		lazy = false,      -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons"
+		}
 	},
 	{ "mg979/vim-visual-multi" }, -- multicursor
 	{ "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" }},
