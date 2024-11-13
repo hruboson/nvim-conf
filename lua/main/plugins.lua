ffdcc545320f8034b9a9b8772ddfb5acb2d6c90d
Ondřej Hruboš <hruboson@gmail.com>
2024-11-13 12:25:33 +0100

    markview doesn't work on windows

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -93,16 +93,16 @@ require("lazy").setup({ -- Enable Lazy
 
 	{ "b0o/mapx.nvim" }, -- easy keybinds
 	{ "dstein64/nvim-scrollview"}, -- scrollbar
-	{ -- Markdown preview
-		"OXY2DEV/markview.nvim",
-		lazy = false,      -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
-
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons"
-		}
-	},
+	--{ -- Markdown preview
+	--	"OXY2DEV/markview.nvim",
+	--	lazy = false,      -- Recommended
+	--	-- ft = "markdown" -- If you decide to lazy-load anyway
+	--
+	--	dependencies = {
+	--		"nvim-treesitter/nvim-treesitter",
+	--		"nvim-tree/nvim-web-devicons"
+	--	}
+	--},
 	{ "mg979/vim-visual-multi" }, -- multicursor
 	{ "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" }},
 	{ "LunarVim/bigfile.nvim" },
