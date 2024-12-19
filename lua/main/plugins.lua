c1c50e59ab79a0470c817739c8b5ea265d2e0614
Ondřej Hruboš <hruboson@gmail.com>
2024-12-19 14:19:26 +0100

    jupyter python notebooks

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -122,8 +122,18 @@ require("lazy").setup({ -- Enable Lazy
 
 
 	--------------------- IDE (AUTOCOMPLETE, LSP, ...) ---------------------
-	{ 'echasnovski/mini.completion', version = '*' }, -- autocomplete
-
+	{ "echasnovski/mini.completion", version = "*" }, -- autocomplete
+	{ -- jupiter notebooks
+		"kiyoon/jupynium.nvim",
+		build = "pip3 install --user .",
+		run="pip3 install --user jupynium",
+		-- build = "conda run --no-capture-output -n jupynium pip install .",
+		-- enabled = vim.fn.isdirectory(vim.fn.expand "~/miniconda3/envs/jupynium"),
+		dependencies = {
+			"rcarriga/nvim-notify",   -- optional
+			"stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
+		},
+	},
 
 
 
