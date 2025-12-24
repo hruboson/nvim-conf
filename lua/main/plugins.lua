53daff4d5fa1219c090e52d729ed7a16f9e60554
hruboson <hruboson@gmail.com>
2025-12-24 13:58:47 +0100

    Revert "Fix treesitter config"
    This reverts commit 05fd6988869bec4ff6ebc97cb5ae1a7c6442ea61.

--- lua/main/plugins.lua
+++ lua/main/plugins.lua
@@ -49,71 +49,7 @@ require("lazy").setup({ -- Enable Lazy
 		},
 		config = true
 	},
-	{ -- syntax hightlighting parser packager 
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- enable syntax highlighting
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = true,
-				},
-
-				-- enable indentation
-				indent = { enable = false },
-
-				-- ensure these language parsers are installed
-				-- ensure_installed = { this shit doesnt work on windows and is super slow on startup
-				--   "c",
-				--   "cpp",
-				--   "python",
-				--   "json",
-				--   "javascript",
-				--   "html",
-				--   "css",
-				--   "markdown",
-				--   "markdown_inline",
-				--   "bash",
-				--   "lua",
-				--   "vim"
-				-- },
-
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = 'o',
-						toggle_hl_groups = 'i',
-						toggle_injected_languages = 't',
-						toggle_anonymous_nodes = 'a',
-						toggle_language_display = 'I',
-						focus_language = 'f',
-						unfocus_language = 'F',
-						update = 'R',
-						goto_node = '<cr>',
-						show_help = '?',
-					},
-				},
-			})
-
-			-- set specific highlighting
-			vim.api.nvim_set_hl(0, "@type", { link = "Keyword" })
-
-			-- set specific parser for filetypes
-			vim.filetype.add({
-				extension = {
-					jison = "jison",
-					jisonlex = "jisonlex",
-				}
-			})
-
-			vim.treesitter.language.register("javascript", "jison")
-			vim.treesitter.language.register("javascript", "jisonlex")
-		end,
-	},
+	{ "nvim-treesitter/nvim-treesitter" }, -- syntax highlighting parser packager
 	{ "windwp/nvim-autopairs" }, -- Bracket autopairs
 	{ "lukas-reineke/indent-blankline.nvim" }, -- Leading lines for indentation
 	{ "nvim-lualine/lualine.nvim" }, -- Bottom statusline
